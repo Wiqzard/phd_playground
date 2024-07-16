5c3dba0b7665792b0ece35993a4b128999b8f501
Stapf Sebastian (ss24m050) <ss24m050@vnode08.cluster>
2024-07-16 18:30:47 +0200

    Added example video functionality

---
+++ src/models/components/video_test.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super(Down, self).__init__()
+        self.maxpool_conv = nn.Sequential(nn.MaxPool2d(2), DoubleConv(in_channels, out_channels))
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super(Up, self).__init__()
+
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(
+                in_channels // 2, in_channels // 2, kernel_size=2, stride=2
+            )
+
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class SmallerVideoUNet(nn.Module):
+    def __init__(self, n_channels, input_length, output_length, bilinear=True):
+        super(SmallerVideoUNet, self).__init__()
+        self.input_length = input_length
+        self.output_length = output_length
+        self.n_channels = n_channels
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels * input_length, 16)
+        self.down1 = Down(16, 32)
+        self.down2 = Down(32, 64)
+        self.down3 = Down(64, 128)
+        self.down4 = Down(128, 128)
+        self.up1 = Up(256, 64, bilinear)
+        self.up2 = Up(128, 32, bilinear)
+        self.up3 = Up(64, 16, bilinear)
+        self.up4 = Up(32, 16, bilinear)
+        self.outc = OutConv(16, n_channels * output_length)
+
+    def forward(self, x):
+        x = x = x.view(x.size(0), -1, x.size(3), x.size(4))
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        out = self.outc(x)
+        return out.view(x.size(0), self.output_length, self.n_channels, x.size(2), x.size(3))
+
+
+# Example usage:
+if __name__ == "__main__":
+    model = SmallerVideoUNet(n_channels=3, input_length=5, output_length=15)
+    input_tensor = torch.randn(1, 3, 5, 128, 128)  # batch_size, channels, height, width
+    output = model(input_tensor)
+    print(output.shape)  # should be [1, 3, 5, 128, 128]
